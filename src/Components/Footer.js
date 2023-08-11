48e49340940be4ba1f60247bf433fbff8bf7411c
Sirkew <Sirkew2020@gmail.com>
2023-08-11 19:40:28 +0600

    adding new

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,10 +9,10 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
+          <Col size={12} sm={4}>
             <img src={logo} alt="Logo" />
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={12} sm={8} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.facebook.com/profile.php?id=100093679211654"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://www.linkedin.com/in/sirkew-chakma-b400851b8/"><img src={navIcon2} alt="Icon" /></a>
